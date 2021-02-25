3942b23d1b52fc92ebf08f7766cdb60a5224f8d3
Sebastian Montoya <seb246@gmail.com>
2021-02-24 19:25:38 -0500

    iteration 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import foods from './foods.json';
-import FoodBox from './components/FoodBox'
+import FoodBox from './components/FoodBox';
 
-function App() {
-  return (
-    <div className="App">
-    <FoodBox/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    allFoods: foods,
+  };
+
+  showFoods = () => {
+    return this.state.allFoods.map((eachFood) => {
+
+      return <FoodBox {...eachFood} />;
+    });
+  };
+
+  render() {
+    return <div className="App">{this.showFoods()}</div>;
+  }
 }
 
 export default App;
